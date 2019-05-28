77cbec6da04a01ef3a7007cd335e078c5813887b
Frantiesco Masutti <tiesco.masutti@swrve.com>
2019-05-28 14:08:45 +0100

    Removed the basic integration hook regarding it was moved to our SwrvePlugin.

--- template_src/hooks/after_platform_add/ios-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#search for the "AppDelegate" and replace for the SwrveAppDelegate integrated Template.
-find platforms/ios/ -name "AppDelegate.m" -exec cp -fr integration-utils/ios/AppDelegate.m {} \;
